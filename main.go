8a86023ca912be31933cf3aa760d64231dbb800c
Avinash K <avikalar@in.ibm.com>
2015-01-22 16:57:09 +0530

    Error Handling

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     //"time"
     "database/sql"
     "strings"
+	"log"
     _ "bitbucket.org/phiggins/db2cli"
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 )
@@ -19,7 +20,9 @@ func main() {
 
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
-      fmt.Fprintln(res, "Error occurred while processing request --> ", err )
+      //panic(err)
+	  log.Fatal(err);
+	  fmt.Println("Error processing request ", err, " ...")
     }
 }
 
@@ -34,59 +37,63 @@ func connect(res http.ResponseWriter, req *http.Request) {
 	
 	// Fetch dashDB service details
 	appEnv, _ := cfenv.Current()
-	services, _ := appEnv.Services.WithLabel("dashDB")
+	services, error := appEnv.Services.WithLabel("dashDB")
 	
-	dashDB := services[0]
-	
-	connStr := []string{"DATABASE=", dashDB.Credentials["db"], ";", "HOSTNAME=", dashDB.Credentials["hostname"], ";", 
-			"PORT=", dashDB.Credentials["port"], ";", "PROTOCOL=TCPIP", ";", "UID=", dashDB.Credentials["username"], ";", "PWD=", dashDB.Credentials["password"]};
-	conn := strings.Join(connStr, "")
-	
-	db, err := sql.Open("db2-cli", conn)
-	if err != nil {
+	if( services != nil ) {
+		dashDB := services[0]
+		
+		connStr := []string{"DATABASE=", dashDB.Credentials["db"], ";", "HOSTNAME=", dashDB.Credentials["hostname"], ";", 
+				"PORT=", dashDB.Credentials["port"], ";", "PROTOCOL=TCPIP", ";", "UID=", dashDB.Credentials["username"], ";", "PWD=", dashDB.Credentials["password"]};
+		conn := strings.Join(connStr, "")
+		
+		db, err := sql.Open("db2-cli", conn)
+		if err != nil {
 
-		fmt.Fprintln(res, "<h3>" )
-		fmt.Fprintln(res, err )
-		fmt.Fprintln(res, "</h3>" )
-		return
-	}
-	
-	defer db.Close()
-	
-	var (
-		first_name string
-		last_name string
-	)
-	
-	stmt, err := db.Prepare("SELECT FIRST_NAME, LAST_NAME from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY")
-	if err != nil {
-		fmt.Fprintln(res, "<h3>" )
-		fmt.Fprintln(res, err )
-		fmt.Fprintln(res, "</h3>" )
-		return
-	}
-	defer stmt.Close()
-	
-	rows, err := stmt.Query()
-    if err != nil {
-        fmt.Fprintln(res, "<h3>" )
-		fmt.Fprintln(res, err )
-		fmt.Fprintln(res, "</h3>" )
-		return
-    }
-    defer rows.Close()
-	fmt.Fprintln(res, "<h3>Query: <br>SELECT FIRST_NAME, LAST_NAME from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY</h3>" )
-	fmt.Fprintln(res, "<h3>Result:<br></h3><table border=\"1\"><tr><th>First Name</th><th>Last Name</th></tr>" )
-    for rows.Next() {
-		err := rows.Scan(&first_name, &last_name)
+			fmt.Fprintln(res, "<h3>" )
+			fmt.Fprintln(res, err )
+			fmt.Fprintln(res, "</h3>" )
+			return
+		}
+		
+		defer db.Close()
+		
+		var (
+			first_name string
+			last_name string
+		)
+		
+		stmt, err := db.Prepare("SELECT FIRST_NAME, LAST_NAME from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY")
 		if err != nil {
 			fmt.Fprintln(res, "<h3>" )
 			fmt.Fprintln(res, err )
 			fmt.Fprintln(res, "</h3>" )
-			return;
-			
+			return
 		}
-		fmt.Fprintln(res, "<tr><td>", first_name, "</td><td>",last_name, "</td></tr>")
+		defer stmt.Close()
+		
+		rows, err := stmt.Query()
+		if err != nil {
+			fmt.Fprintln(res, "<h3>" )
+			fmt.Fprintln(res, err )
+			fmt.Fprintln(res, "</h3>" )
+			return
+		}
+		defer rows.Close()
+		fmt.Fprintln(res, "<h3>Query: <br>SELECT FIRST_NAME, LAST_NAME from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY</h3>" )
+		fmt.Fprintln(res, "<h3>Result:<br></h3><table border=\"1\"><tr><th>First Name</th><th>Last Name</th></tr>" )
+		for rows.Next() {
+			err := rows.Scan(&first_name, &last_name)
+			if err != nil {
+				fmt.Fprintln(res, "<h3>" )
+				fmt.Fprintln(res, err )
+				fmt.Fprintln(res, "</h3>" )
+				return;
+				
+			}
+			fmt.Fprintln(res, "<tr><td>", first_name, "</td><td>",last_name, "</td></tr>")
+		}
+	} else {
+		fmt.Fprintln(res, "<h4>No dashDB service instance bound to the applicaiton. Please bind a dashDB service instance and retry.</h4>")
 	}
 	fmt.Fprintln(res, "</table></body></html>")
 }
